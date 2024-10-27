b71e1ab49eb0d41781a4b6236da556781c0020c2
Luiz Felipe <gilberto.barbosa@7comm.com.br>
2024-10-27 18:17:22 -0300

    FIX[Bug-2]: Adiciona migration de criação das views para o dashboard

---
+++ bugs/Dima.Api/Data/Migrations/20241027205622_ExecuteViews.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dima.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExecuteViews : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sqlDirectory = Path.Combine("Data/Views");
+
+            var sqlFiles = new string[]
+            {
+                "vwGetExpensesByCategory.sql",
+                "vwGetIncomesAndExpenses.sql",
+                "vwGetIncomesByCategory.sql"
+            };
+
+            foreach (var item in sqlFiles)
+            {
+                var filePath = Path.Combine(sqlDirectory, item);
+                var sqlQuery = File.ReadAllText(filePath);
+                migrationBuilder.Sql(sqlQuery);
+            }
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP VIEW vwGetExpensesByCategory");
+            migrationBuilder.Sql("DROP VIEW vwGetIncomesAndExpenses");
+            migrationBuilder.Sql("DROP VIEW vwGetIncomesByCategory");
+        }
+    }
+}
